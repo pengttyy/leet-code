0ef5bffbcbb08cde3c155ff0353daac0b634adf0
pengttyy <pengttyy@gmail.com>
2020-07-17 10:11:51 +0800

    [多线程](1114): 按序打印

---
+++ src/main/java/com/object/leetcode/t1114/Solution1114.java
@@ -0,0 +1,37 @@
+package com.object.leetcode.t1114;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author kai.peng
+ */
+public class Solution1114 {
+    private CountDownLatch firstSecond = new CountDownLatch(1);
+    private CountDownLatch secondThird = new CountDownLatch(1);
+
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        printFirst.run();
+        firstSecond.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        firstSecond.await();
+        printSecond.run();
+        secondThird.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        secondThird.await();
+        printThird.run();
+    }
+
+    public static void main(String[] args) {
+        int num = 0;
+        num = (num + 1) % 3;
+
+        System.out.println(num);
+    }
+}
